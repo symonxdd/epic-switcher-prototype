51e5ac2969ff4d03c5416f012c7db8eee9cdef4b
Symon <symon.bla@gmail.com>
2023-03-11 04:01:28 +0100

    Added all functionality
    - Yes this commit has too many code changes, this because I programmed beforehand without committing 😅
    
    - Some polishing will definitely follow, like:
    - Add feature
    - Automagically use the account name, instead of its Account ID
    - Tried to implement a tray icon, so a user can switch accounts and see to which account he's logged in, but there's no WinAppSDK native way to do this.
    There's a promising-enough library for this: https://github.com/HavenDV/H.NotifyIcon, but it seems to be lacking the ability to add other UI controls than basic Buttons / Commands. So a ListBox is not possible, too bad.
    Will yet look into though - feels like a really nice feature to have

--- EpicGamesAccountSwitcher/MainWindow.xaml.cs
+++ EpicGamesAccountSwitcher/MainWindow.xaml.cs
@@ -2,6 +2,12 @@ using Microsoft.UI.Windowing;
 using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using System;
+using RegistryUtils;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace EpicGamesAccountSwitcher
 {
@@ -10,10 +16,156 @@ namespace EpicGamesAccountSwitcher
         private AppWindow _appWindow;
         private OverlappedPresenter _presenter;
 
+        private const string authDataFolder = @"C:\Users\Symon\Desktop\AuthData\";
+        private const string configFolder = @"C:\Users\Symon\AppData\Local\EpicGamesLauncher\Saved\Config\Windows\";
+        private const string regKeyPath = @"HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Identifiers";
+        private const string regKeyName = "AccountId";
+        private const string epicGamesExecPath = @"C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe";
+
+        // Source: https://www.codeproject.com/Articles/4502/RegistryMonitor-a-NET-wrapper-class-for-RegNotifyC
+        private readonly RegistryMonitor _registryMonitor;
+
+        //string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
         public MainWindow()
         {
             InitializeComponent();
             SetupBasicFeatures();
+
+            PopulateAccountListBox();
+            accountIdTextBlock.Text = GetCurrentAccountId();
+
+            _registryMonitor = new RegistryMonitor(RegistryHive.CurrentUser, "Software\\Epic Games\\Unreal Engine\\Identifiers");
+            _registryMonitor.RegChanged += new EventHandler(OnRegistryChanged);
+        }
+
+        private string GetCurrentAccountId()
+        {
+            return (string)Registry.GetValue(regKeyPath, regKeyName, null);
+        }
+
+        private string GetSelectedAccountId()
+        {
+            return (string)accountListBox.SelectedValue;
+        }
+
+        private void PopulateAccountListBox()
+        {
+            accountListBox.Items.Clear();
+            string[] files = Directory.GetFiles(authDataFolder);
+
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                accountListBox.Items.Add(fileName);
+            }
+            accountListBox.SelectedIndex = 0;
+        }
+
+        private async Task<ContentDialogResult> ShowContentDialog(string title, string content)
+        {
+            ContentDialog dialog = new()
+            {
+                XamlRoot = mainWindow.Content.XamlRoot,
+                Title = title,
+                Content = content,
+                PrimaryButtonText = "Yes",
+                SecondaryButtonText = "No",
+                DefaultButton = ContentDialogButton.Primary,
+            };
+            return await dialog.ShowAsync();
+        }
+
+        private async void ChangeAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await ShowContentDialog("Switch?", "Are you sure you want to switch accounts?");
+            if (result == ContentDialogResult.Primary)
+            {
+                CloseEpicGamesLauncher();
+                ChangeAccount();
+                StartEpicGamesLauncher();
+            }
+            else { return; }
+        }
+
+        private void CloseEpicGamesLauncher()
+        {
+            string processName = "EpicGamesLauncher";
+
+            Process[] processes = Process.GetProcessesByName(processName);
+            if (processes.Length != 0)
+            {
+                Process process = processes[0];
+                process.Kill();
+                process.WaitForExit();
+            }
+        }
+
+        private void StartEpicGamesLauncher()
+        {
+            Process.Start(epicGamesExecPath);
+        }
+
+
+        private void ChangeAccount()
+        {
+            SwapFiles();
+            ChangeRegKey();
+            UpdateUi();
+        }
+
+        private void UpdateUi()
+        {
+            PopulateAccountListBox();
+            accountIdTextBlock.Text = GetCurrentAccountId();
+        }
+
+        private void SwapFiles()
+        {
+            string configFileName = "GameUserSettings.ini";
+
+            string fileToMove = $"{configFolder}{configFileName}";
+            string newFileName = $"{GetCurrentAccountId()}.ini";
+            string newFilePath = Path.Combine(authDataFolder, newFileName);
+            File.Move(fileToMove, newFilePath);
+
+            fileToMove = $"{authDataFolder}{GetSelectedAccountId()}.ini";
+            newFileName = configFileName;
+            newFilePath = Path.Combine(configFolder, newFileName);
+            File.Move(fileToMove, newFilePath);
+        }
+
+        private void ChangeRegKey()
+        {
+            Registry.SetValue(regKeyPath, "AccountId", GetSelectedAccountId());
+        }
+
+        private async void NewAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await ShowContentDialog("Add?", "Are you sure you want to log into another account?");
+            if (result == ContentDialogResult.Primary)
+            {
+                CloseEpicGamesLauncher();
+
+                string configFileName = "GameUserSettings.ini";
+                string fileToMove = $"{configFolder}{configFileName}";
+                string newFileName = $"{GetCurrentAccountId()}.ini";
+                string newFilePath = Path.Combine(authDataFolder, newFileName);
+                File.Move(fileToMove, newFilePath);
+
+                StartEpicGamesLauncher();
+                _registryMonitor.Start();
+            }
+            else { return; }
+        }
+
+        private void OnRegistryChanged(object sender, EventArgs e)
+        {
+            _registryMonitor.Stop();
+            //Dispatcher.Invoke(() =>
+            //{
+            //    UpdateUi();
+            //});
         }
 
         private void SetupBasicFeatures()
